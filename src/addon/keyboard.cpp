12e64ef11f80094962ba80572a799eca9da985ca
Ryan Pavlik <rpavlik@gmail.com>
2023-01-28 15:55:13 -0600

    Support sending numeric scancodes

--- src/addon/keyboard.cpp
+++ src/addon/keyboard.cpp
@@ -3,7 +3,11 @@
 #include "helper.hpp"
 
 void Keyboard::toggleKey(const Napi::CallbackInfo &info) {
-  keyToggler(Helper::keyboardButtons.at(info[0].As<Napi::String>()), info[1].As<Napi::Boolean>());
+  if (info[0].IsNumber())
+    keyToggler(info[0].As<Napi::Number>(), info[1].As<Napi::Boolean>());
+  else
+    keyToggler(Helper::keyboardButtons.at(info[0].As<Napi::String>()),
+               info[1].As<Napi::Boolean>());
 }
 
 void Keyboard::printChar(const Napi::CallbackInfo &info) {
