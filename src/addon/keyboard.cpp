68f8ebafde88d27469c0074604b3a87336482196
Ryan Pavlik <rpavlik@gmail.com>
2023-01-29 06:11:19 -0600

    Support sending numeric scancodes (#19)
    * Support sending numeric scancodes
    
    * Reformat / ternary
    
    clang-format's default choice here isn't amazing, but the alternatives
    aren't much better. ;/

--- src/addon/keyboard.cpp
+++ src/addon/keyboard.cpp
@@ -3,7 +3,11 @@
 #include "helper.hpp"
 
 void Keyboard::toggleKey(const Napi::CallbackInfo &info) {
-  keyToggler(Helper::keyboardButtons.at(info[0].As<Napi::String>()), info[1].As<Napi::Boolean>());
+  UINT code = info[0].IsNumber()
+                  ? info[0].As<Napi::Number>()
+                  : Helper::keyboardButtons.at(info[0].As<Napi::String>());
+
+  keyToggler(code, info[1].As<Napi::Boolean>());
 }
 
 void Keyboard::printChar(const Napi::CallbackInfo &info) {
